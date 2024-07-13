8b7d1786e940797a1d1ef6d3724287fbbdeaf3e3
Cory Frenette <cory@frenette.dev>
2024-07-13 14:27:12 -0400

    add nix shell aliases

--- home/modules/tui/shell/aliases.nix
+++ home/modules/tui/shell/aliases.nix
@@ -1,4 +1,8 @@
 {
-  # TODO: Shell Aliases
+  nixgc = "nh clean all --keep 5";
+  nixu = "'nix flake update --flake $FLAKE'";
+  nixb = "nh os build";
+  nixs = "nh os switch";
+  nixe = "cd $FLAKE && nvim && cd -";
 }
 
