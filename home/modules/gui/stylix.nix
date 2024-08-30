97599fd61bd5bc5f91bc48789f4c931cd9a37b66
Cory Frenette <cory@frenette.dev>
2024-08-30 19:53:27 -0400

    add chrome and mattermost to gui module

--- home/modules/gui/stylix.nix
+++ home/modules/gui/stylix.nix
@@ -2,6 +2,7 @@
   stylix = {
     enable = true;
     targets.nixvim.enable = false;
+    opacity.terminal = 0.8;
   };
 }
 
--- home/modules/tui/nixvim/plugins/default.nix
+++ home/modules/tui/nixvim/plugins/default.nix
@@ -14,6 +14,7 @@
     extraPlugins = [ pkgs.vimPlugins.gruvbox-material ];
     extraConfigLua = ''
       vim.g.gruvbox_material_background = 'hard';
+      vim.g.gruvbox_material_transparent_background = 1;
       vim.g.gruvbox_material_foreground = 'material';
       vim.g.gruvbox_material_ui_contrast = 'high';
       vim.g.gruvbox_material_diagnostic_line_highlight = 1;
