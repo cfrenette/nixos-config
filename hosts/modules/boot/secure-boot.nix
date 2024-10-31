0030e7c98900fc47309a4fc6a59390d23705a6b4
Cory Frenette <cory@frenette.dev>
2024-10-31 13:52:12 -0400

    try overlay planes patch

--- hosts/modules/boot/secure-boot.nix
+++ hosts/modules/boot/secure-boot.nix
@@ -1,10 +1,21 @@
-{ pkgs, lib, inputs, ... }:
+{ pkgs, config, lib, inputs, ... }:
+let
+  amdgpu-kernel-module = pkgs.callPackage ../../../patches/amdgpu.nix {
+    kernel = config.boot.kernelPackages.kernel;
+  };
+in
 {
   imports = [
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_10;
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    extraModulePackages = [
+      (amdgpu-kernel-module.overrideAttrs (_: {
+        patches = [ ../../../patches/0001-fix-dirty-patch-for-overlay-planes-bug.patch ];
+      }))
+    ];
 
     loader = {
       # Replaced by lanzaboote
