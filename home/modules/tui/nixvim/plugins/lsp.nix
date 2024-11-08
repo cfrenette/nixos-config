0403f6bee367bb096ab8b7cc6101dc971853322b
Cory Frenette <cory@frenette.dev>
2024-11-08 18:05:29 -0500

    add lsp support for c, configure keybind for vim.lsp.buf.rename()

--- home/modules/tui/nixvim/plugins/lsp.nix
+++ home/modules/tui/nixvim/plugins/lsp.nix
@@ -19,6 +19,7 @@
         "gi" = "implementation";
         "gt" = "type_definition";
         "<M-CR>" = "code_action";
+        "<leader>r" = "rename";
       };
     };
 
@@ -27,9 +28,15 @@
         enable = true;
         settings.formatting.command = [ "nixpkgs-fmt" ];
       };
+      clangd = {
+        enable = true;
+      };
       bashls.enable = true;
       rust-analyzer = {
         enable = true;
+        # Use the more up-to-date versions in a devshell
+        installRustc = false;
+        installCargo = false;
       };
       tsserver.enable = true;
       yamlls.enable = true;
