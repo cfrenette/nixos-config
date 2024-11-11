6b971dc70b77d0c27d4f8a4464a2dd255491c5f2
Cory Frenette <cory@frenette.dev>
2024-11-11 00:20:38 -0500

    upgrade to 6.12-rc7

--- hosts/modules/boot/secure-boot.nix
+++ hosts/modules/boot/secure-boot.nix
@@ -14,11 +14,11 @@ in
         linux_6_12_pkg = { fetchurl, buildLinux, ... } @ args:
           buildLinux (args // rec {
             src = fetchurl {
-              url = "https://git.kernel.org/torvalds/t/linux-6.12-rc6.tar.gz";
-              sha256 = "sha256-eTJwO784WZMvYVwh3kxa5W2Z3IVSW8I2HVQyFs/35Mo=";
+              url = "https://git.kernel.org/torvalds/t/linux-6.12-rc7.tar.gz";
+              sha256 = "sha256-hYHy81vx++0nGt1a9zz9iMKZorO+RmgRARaYdEPKo2E=";
             };
             dontStrip = true;
-            version = "6.12.0-rc6";
+            version = "6.12.0-rc7";
             modDirVersion = version;
           } // (args.argsOverride or { }));
         linux_6_12 = pkgs.callPackage linux_6_12_pkg { };
@@ -29,7 +29,7 @@ in
     extraModulePackages = [
       (amdgpu-kernel-module.overrideAttrs (_: {
         patches = [
-          # ../../../patches/amdgpu_mst.patch
+          ../../../patches/mst-patch_6-12-rc6.patch
           ../../../patches/overlay-planes-patch_6-12-rc6.patch
         ];
       }))
