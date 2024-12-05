a8bd6cb401b2a4b9bee4ebd928f49b0c5c0c11a7
Cory Frenette <cory@frenette.dev>
2024-12-05 18:47:20 -0500

    configure git sendmail

--- home/cory/git.nix
+++ home/cory/git.nix
@@ -1,9 +1,10 @@
-{
+{ pkgs, config, ... }: {
   # Allow Signing with SSH Key
   home.file.".ssh/allowed_signers".text = "cory@frenette.dev ${builtins.readFile ./keys/id_ed25519.pub}";
 
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
 
     userName = "Cory Frenette";
     userEmail = "cory@frenette.dev";
@@ -15,13 +16,38 @@
       gpg.ssh.allowedSignersFile = "/home/cory/.ssh/allowed_signers";
       user.signingKey = "/home/cory/.ssh/id_ed25519.pub";
       init.defaultBranch = "main";
+      # Sendemail
+      sendemail.sendmailCmd = "${pkgs.msmtp}/bin/sendmail";
     };
+  };
+
+  # Sendmail
+  programs.msmtp = {
+    enable = true;
+  };
+
+  # Configure Sendmail
+  accounts.email.accounts.cory = {
+    msmtp.enable = true;
+    primary = true;
+    realName = "Cory Frenette";
+    smtp = {
+      tls = {
+        enable = true;
+        useStartTls = false;
+      };
+      host = "smtp.gmail.com";
+    };
+    address = "cory@frenette.dev";
+    userName = "frenette.cory@gmail.com";
+    passwordCommand = "cat ${config.sops.secrets."users/cory/gmail".path}";
   };
 
   sops.secrets = {
     "users/cory/pgp" = {
       path = "/home/cory/.ssh/pgp_private.asc";
     };
+    "users/cory/gmail" = { };
   };
 }
 
