1aeddcdd277ddeb4c9bdb7b990589ef0ae2b1546
Cory Frenette <cory@frenette.dev>
2025-10-29 23:55:05 -0400

    pin nixpkgs to 5900907 to avoid NixOS/nixos-hardware#1655

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/5900907";
 
     # NixOS Hardware Profiles
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
