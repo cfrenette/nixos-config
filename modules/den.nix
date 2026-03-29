dabe6b698a3f530c198ac32122f0e7faf3ef7a83
Cory Frenette <cory@frenette.dev>
2026-03-29 18:04:50 -0400

    fix: make frmwrk, wsl actually enable user config

--- modules/den.nix
+++ modules/den.nix
@@ -1,7 +1,17 @@
-{ inputs, lib, ... }:
+{
+  den,
+  inputs,
+  lib,
+  ...
+}:
 {
   imports = [ inputs.den.flakeModule ];
 
   den.schema.user.classes = lib.mkDefault [ "homeManager" ];
+
+  den.ctx.user.includes = [
+    # Enable host<->user config via .provides
+    den._.mutual-provider
+  ];
 
 }
