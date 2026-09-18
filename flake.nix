8a920f03fbd416d19beec80164f34dee66bc01e2
Cory Frenette <cory@frenette.dev>
2026-09-18 01:54:34 -0400

    fix: workaround for opencode circular import: anomalyco/opencode#48372

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# DO-NOT-EDIT. This file was auto-generated using github:denful/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
@@ -14,7 +14,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,6 +30,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:cfrenette/nixvim";
+    opencode-pr.url = "github:NixOS/nixpkgs/pull/564101/head";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/aspects/tui/opencode.nix
+++ modules/aspects/tui/opencode.nix
@@ -1,11 +1,20 @@
+{ den, inputs, ... }:
 {
+  flake-file.inputs.opencode-pr = {
+    url = "github:NixOS/nixpkgs/pull/564101/head";
+  };
   den.aspects.opencode = {
+    nixos.nixpkgs.overlays = [
+      (final: prev: {
+        opencode = final.callPackage "${inputs.opencode-pr}/pkgs/by-name/op/opencode/package.nix" { };
+      })
+    ];
     homeManager = {
       programs.opencode = {
         enable = true;
         settings = {
           "$schema" = "https://opencode.ai/config.json";
-          plugin = [ "@ex-machina/opencode-anthropic-auth" ];
+          plugin = [ "@ex-machina/opencode-anthropic-auth@1.8.4" ];
           lsp = { };
           disabled_providers = [ "opencode" ];
         };
